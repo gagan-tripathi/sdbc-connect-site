c2cbc4a9303e87d3dfbeff1170b2fe6fc0f85506
Gagan Tripathi <gagantripathi22@gmail.com>
2021-07-09 23:00:01 +0530

    hero image changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,5 @@
-/**
-* Template Name: Ninestars - v4.3.0
-* Template URL: https://bootstrapmade.com/ninestars-free-bootstrap-3-theme-for-creative/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
-
-  /**
-   * Easy selector helper function
-   */
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
-  }
-
-  /**
-   * Easy event listener function
-   */
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
-      } else {
-        selectEl.addEventListener(type, listener)
-      }
-    }
-  }
-
   /**
    * Easy on scroll event listener 
    */
